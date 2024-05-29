d22b0bb9d4d679a8a01f91802b033cd3f7b13888
Ahmed Abdalla Abdelrehim <aabdelre@redhat.com>
2024-05-29 16:16:23 +0200

    Force remove tmp files when running tests (#71)
    Without the -f, run_tests.sh throws an error that the files
    don't exist.
    
    Signed-off-by: Ahmed Abdalla <ahmed@devguy.io>

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm testutils/*.txt; rm testutils/*.lock
+rm -f testutils/*.txt; rm -f testutils/*.lock
 pkill etcd
 date > test-results.log
 time go test ./... -count=1 -race -failfast -timeout 10m 2>&1 | tee -a test-results.log
