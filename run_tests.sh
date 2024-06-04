e2fb06848596e835cb5fb11d7c90f9be40b0d127
Ahmed Abdalla <ahmed@devguy.io>
2024-06-04 21:51:54 +0200

    Set integration build tag for integration tests
    Signed-off-by: Ahmed Abdalla <ahmed@devguy.io>

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 date > test-results.log
-time go test ./... -count=1 -race -failfast -timeout 10m 2>&1 | tee -a test-results.log
+time gotestsum -f testname -- -count=1 -race -failfast -timeout 10m ./... 2>&1 | tee -a test-results.log
+time gotestsum -f testname -- -count=1 -race -failfast -timeout 10m -tags=integration ./integration/... 2>&1 | tee -a test-results.log
