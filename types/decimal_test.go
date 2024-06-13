45e71b65aa1219ed49467f8ed8fd6dcbce3fee5c
Ahmed Abdalla Abdelrehim <ahmed@devguy.io>
2024-06-13 14:05:34 +0200

    Refactor decimal comparison and arithmetic tests and add -ve nums (#85)
    * Refactor decimal comparison tests and add -ve nums
    
    Signed-off-by: Ahmed Abdalla <ahmed@devguy.io>
    
    * Use table tests for Decimal arithmetic operations
    
    Signed-off-by: Ahmed Abdalla <ahmed@devguy.io>
    
    ---------
    
    Signed-off-by: Ahmed Abdalla <ahmed@devguy.io>

--- types/decimal_test.go
+++ types/decimal_test.go
@@ -8,269 +8,318 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestDecimal_Equals(t *testing.T) {
-	type d struct {
-		Num       int64
-		Precision int
-		Scale     int
+func TestDecimalComparison(t *testing.T) {
+	type Want struct {
+		Equal           bool
+		LessThan        bool
+		LessOrEquals    bool
+		GreaterThan     bool
+		GreaterOrEquals bool
 	}
 	tests := []struct {
 		name string
-		d1   d
-		d2   d
-		want bool
+		d1   Decimal
+		d2   Decimal
+		want Want
 	}{
 		{
-			name: "same precision and scale and equal values (+ve)",
-			d1:   d{123421, 10, 2},
-			d2:   d{123421, 10, 2},
-			want: true,
+			name: "SamePrecScale__(+A)<(+B)",
+			d1:   Dec(123421, 10, 2),
+			d2:   Dec(456456, 10, 2),
+			want: Want{
+				Equal:           false,
+				LessThan:        true,
+				LessOrEquals:    true,
+				GreaterThan:     false,
+				GreaterOrEquals: false,
+			},
 		},
 		{
-			name: "same precision and scale and not equal values (+ve)",
-			d1:   d{123421, 10, 2},
-			d2:   d{456456, 10, 2},
-			want: false,
+			name: "SamePrecScale__(+A)>(+B)",
+			d1:   Dec(456456, 10, 2),
+			d2:   Dec(123421, 10, 2),
+			want: Want{
+				Equal:           false,
+				LessThan:        false,
+				LessOrEquals:    false,
+				GreaterThan:     true,
+				GreaterOrEquals: true,
+			},
 		},
 		{
-			name: "same precision and scale and equal values (-ve)",
-			d1:   d{-123421, 10, 2},
-			d2:   d{-123421, 10, 2},
-			want: true,
+			name: "SamePrecScale__(+A)==(+B)",
+			d1:   Dec(123421, 10, 2),
+			d2:   Dec(123421, 10, 2),
+			want: Want{
+				Equal:           true,
+				LessThan:        false,
+				LessOrEquals:    true,
+				GreaterThan:     false,
+				GreaterOrEquals: true,
+			},
 		},
 		{
-			name: "same precision and scale and not equal values (-ve)",
-			d1:   d{-123421, 10, 2},
-			d2:   d{-456456, 10, 2},
-			want: false,
+			name: "DifferentPrecScale__(+A)<(+B)",
+			d1:   Dec(123421, 10, 2),
+			d2:   Dec(22342100, 13, 4),
+			want: Want{
+				Equal:           false,
+				LessThan:        true,
+				LessOrEquals:    true,
+				GreaterThan:     false,
+				GreaterOrEquals: false,
+			},
 		},
 		{
-			name: "different precision and scale and equal values (+ve)",
-			d1:   d{123421, 10, 2},
-			d2:   d{12342100, 13, 4},
-			want: true,
+			name: "DifferentPrecScale__(+A)>(+B)",
+			d1:   Dec(123421, 10, 2),
+			d2:   Dec(11342100, 13, 4),
+			want: Want{
+				Equal:           false,
+				LessThan:        false,
+				LessOrEquals:    false,
+				GreaterThan:     true,
+				GreaterOrEquals: true,
+			},
 		},
 		{
-			name: "different precision and scale and not equal values (+ve)",
-			d1:   d{123421, 10, 2},
-			d2:   d{45645600, 13, 4},
-			want: false,
+			name: "DifferentPrecScale__(+A)==(+B)",
+			d1:   Dec(123421, 10, 2),
+			d2:   Dec(12342100, 13, 4),
+			want: Want{
+				Equal:           true,
+				LessThan:        false,
+				LessOrEquals:    true,
+				GreaterThan:     false,
+				GreaterOrEquals: true,
+			},
 		},
 		{
-			name: "different precision and scale and equal values (-ve)",
-			d1:   d{-123421, 10, 2},
-			d2:   d{-12342100, 13, 4},
-			want: true,
+			name: "SamePrecScale__(-A)<(+B)",
+			d1:   Dec(-456456, 10, 2),
+			d2:   Dec(123421, 10, 2),
+			want: Want{
+				Equal:           false,
+				LessThan:        true,
+				LessOrEquals:    true,
+				GreaterThan:     false,
+				GreaterOrEquals: false,
+			},
 		},
 		{
-			name: "different precision and scale and not equal values (-ve)",
-			d1:   d{-123421, 10, 2},
-			d2:   d{-45645600, 13, 4},
-			want: false,
+			name: "SamePrecScale__(-A)<(-B)",
+			d1:   Dec(-456456, 10, 2),
+			d2:   Dec(-123421, 10, 2),
+			want: Want{
+				Equal:           false,
+				LessThan:        true,
+				LessOrEquals:    true,
+				GreaterThan:     false,
+				GreaterOrEquals: false,
+			},
 		},
+		{
+			name: "SamePrecScale__(-A)==(-B)",
+			d1:   Dec(-123421, 10, 2),
+			d2:   Dec(-123421, 10, 2),
+			want: Want{
+				Equal:           true,
+				LessThan:        false,
+				LessOrEquals:    true,
+				GreaterThan:     false,
+				GreaterOrEquals: true,
+			},
+		},
+		{
+			name: "DifferentPrecScale__(-A)<(+B)",
+			d1:   Dec(-123421, 10, 2),
+			d2:   Dec(22342100, 13, 4),
+			want: Want{
+				Equal:           false,
+				LessThan:        true,
+				LessOrEquals:    true,
+				GreaterThan:     false,
+				GreaterOrEquals: false,
+			},
+		},
+		{
+			name: "DifferentPrecScale__(-A)>(-B)",
+			d1:   Dec(-11342100, 13, 4),
+			d2:   Dec(-123421, 10, 2),
+			want: Want{
+				Equal:           false,
+				LessThan:        false,
+				LessOrEquals:    false,
+				GreaterThan:     true,
+				GreaterOrEquals: true,
+			},
+		},
+		{
+			name: "DifferentPrecScale__(-A)==(-B)",
+			d1:   Dec(-123421, 10, 2),
+			d2:   Dec(-12342100, 13, 4),
+			want: Want{
+				Equal:           true,
+				LessThan:        false,
+				LessOrEquals:    true,
+				GreaterThan:     false,
+				GreaterOrEquals: true,
+			},
+		},
+
 	}
 	for _, tc := range tests {
 		t.Run(
 			tc.name, func(t *testing.T) {
-				d1 := createDecimal(tc.d1.Num, tc.d1.Precision, tc.d1.Scale)
-				d2 := createDecimal(tc.d2.Num, tc.d2.Precision, tc.d2.Scale)
-				got := d1.Equals(&d2)
-				if got != tc.want {
-					t.Errorf("Equals() d1 -> d2 = %v, want %v", got, tc.want)
-				}
-				got = d2.Equals(&d1)
-				if got != tc.want {
-					t.Errorf("Equals() d2 -> d1 = %v, want %v", got, tc.want)
-				}
+				d1 := tc.d1
+				d2 := tc.d2
+				require.Equal(t, tc.want.Equal, d1.Equals(&d2), "%s == %s failed", d1.String(), d2.String())
+				require.Equal(t, tc.want.Equal, d2.Equals(&d1), "%s == %s", d2.String(), d1.String())
+
+				require.Equal(t, tc.want.LessThan, d1.LessThan(&d2), "%s < %s", d1.String(), d2.String())
+				require.Equal(
+					t, !tc.want.Equal && !tc.want.LessThan, d2.LessThan(&d1), "%s < %s", d2.String(), d1.String(),
+				)
+
+				require.Equal(
+					t, tc.want.LessOrEquals, d1.LessOrEquals(&d2), "%s <= %s failed", d1.String(), d2.String(),
+				)
+				require.Equal(
+					t, !tc.want.LessOrEquals || tc.want.Equal, d2.LessOrEquals(&d1), "%s <= %s", d2.String(),
+					d1.String(),
+				)
+
+				require.Equal(t, tc.want.GreaterThan, d1.GreaterThan(&d2), "%s > %s", d1.String(), d2.String())
+				require.Equal(
+					t, !tc.want.Equal && !tc.want.GreaterThan, d2.GreaterThan(&d1), "%s > %s", d2.String(), d1.String(),
+				)
+
+				require.Equal(t, tc.want.GreaterOrEquals, d1.GreaterOrEquals(&d2), "%s >= %s", d1.String(), d2.String())
+				require.Equal(
+					t, !tc.want.GreaterOrEquals || tc.want.Equal, d2.GreaterOrEquals(&d1), "%s >= %s", d2.String(),
+					d1.String(),
+				)
 			},
 		)
 	}
 }
 
-func TestLessThanSamePrecScale(t *testing.T) {
-	d1 := createDecimal(123421, 10, 2)
-	d2 := createDecimal(123421, 10, 2)
-	d3 := createDecimal(456456, 10, 2)
-	require.True(t, d1.LessThan(&d3))
-	require.False(t, d3.LessThan(&d1))
-	require.False(t, d1.LessThan(&d2))
-	require.False(t, d2.LessThan(&d1))
-}
+func TestDecimalArithmetic(t *testing.T) {
+	type Want struct {
+		Add      Decimal
+		Subtract Decimal
+		Multiply Decimal
+		Divide   Decimal
+	}
+	tests := []struct {
+		name string
+		d1   Decimal
+		d2   Decimal
+		want Want
+	}{
+		{
+			name: "SamePrecScale__(+A),(+B)",
+			d1:   Dec(1234, 8, 2),
+			d2:   Dec(5678, 8, 2),
+			want: Want{
+				Add:      Dec(6912, 8, 2),
+				Subtract: Dec(-4444, 8, 2),
+				Multiply: Dec(7006652, 8, 4),
+				Divide:   Dec(21, 8, 2),
+			},
+		},
+		{
+			name: "DifferentPrecScale__(+A),(+B)",
+			d1:   Dec(123456, 9, 3),
+			d2:   Dec(789, 3, 2),
+			want: Want{
+				Add:      Dec(131346, 9, 3),
+				Subtract: Dec(115566, 9, 3),
+				Multiply: Dec(97406784, 9, 5),
+				Divide:   Dec(15647, 9, 3),
+			},
+		},
+		{
+			name: "SamePrecScale__(-A),(+B)",
+			d1:   Dec(-1234, 8, 2),
+			d2:   Dec(789, 8, 2),
+			want: Want{
+				Add:      Dec(-445, 8, 2),
+				Subtract: Dec(-2023, 8, 2),
+				Multiply: Dec(-973626, 8, 4),
+				Divide:   Dec(-156, 8, 2),
+			},
+		},
+		{
+			name: "DifferentPrecScale__(-A),(+B)",
+			d1:   Dec(-123456, 9, 3),
+			d2:   Dec(789, 3, 2),
+			want: Want{
+				Add:      Dec(-115566, 9, 3),
+				Subtract: Dec(-131346, 9, 3),
+				Multiply: Dec(-97406784, 9, 5),
+				Divide:   Dec(-15647, 9, 3),
+			},
+		},
+		{
+			name: "SamePrecScale__(-A),(-B)",
+			d1:   Dec(-1234, 8, 2),
+			d2:   Dec(-5678, 8, 2),
+			want: Want{
+				Add:      Dec(-6912, 8, 2),
+				Subtract: Dec(4444, 8, 2),
+				Multiply: Dec(7006652, 8, 4),
+				Divide:   Dec(21, 8, 2),
+			},
+		},
+		{
+			name: "DifferentPrecScale__(-A),(-B)",
+			d1:   Dec(-123456, 9, 3),
+			d2:   Dec(-789, 3, 2),
+			want: Want{
+				Add:      Dec(-131346, 9, 3),
+				Subtract: Dec(-115566, 9, 3),
+				Multiply: Dec(97406784, 9, 5),
+				Divide:   Dec(15647, 9, 3),
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(
+			tc.name, func(t *testing.T) {
+				d1 := tc.d1
+				d2 := tc.d2
+				dr, err := d1.Add(&d2)
+				require.NoError(t, err, "%s + %s", d1.String(), d2.String())
+				require.Equal(t, tc.want.Add, dr, "%s + %s == %s", d1.String(), d2.String(), dr.String())
 
-func TestLessThanDifferentPrecScale(t *testing.T) {
-	d1 := createDecimal(123421, 10, 2)
-	d2 := createDecimal(12342100, 13, 4)
-	d3 := createDecimal(22342100, 13, 4)
-	d4 := createDecimal(11342100, 13, 4)
-	require.False(t, d1.LessThan(&d2))
-	require.False(t, d2.LessThan(&d1))
-	require.True(t, d1.LessThan(&d3))
-	require.False(t, d1.LessThan(&d4))
-	require.True(t, d4.LessThan(&d1))
-}
+				dr, err = d1.Subtract(&d2)
+				require.NoError(t, err, "%s - %s", d1.String(), d2.String())
+				require.Equal(t, tc.want.Subtract, dr, "%s - %s == %s", d1.String(), d2.String(), dr.String())
 
-func TestLessOrEqualsSamePrecScale(t *testing.T) {
-	d1 := createDecimal(123421, 10, 2)
-	d2 := createDecimal(123421, 10, 2)
-	d3 := createDecimal(456456, 10, 2)
-	require.True(t, d1.LessOrEquals(&d3))
-	require.False(t, d3.LessOrEquals(&d1))
-	require.True(t, d1.LessOrEquals(&d2))
-	require.True(t, d2.LessOrEquals(&d1))
-}
+				dr, err = d1.Multiply(&d2)
+				require.NoError(t, err, "%s * %s", d1.String(), d2.String())
+				require.Equal(t, tc.want.Multiply, dr, "%s * %s == %s", d1.String(), d2.String(), dr.String())
 
-func TestLessOrEqualsDifferentPrecScale(t *testing.T) {
-	d1 := createDecimal(123421, 10, 2)
-	d2 := createDecimal(12342100, 13, 4)
-	d3 := createDecimal(22342100, 13, 4)
-	d4 := createDecimal(11342100, 13, 4)
-	require.True(t, d1.LessOrEquals(&d2))
-	require.True(t, d2.LessOrEquals(&d1))
-	require.True(t, d1.LessOrEquals(&d3))
-	require.False(t, d1.LessOrEquals(&d4))
-	require.True(t, d4.LessOrEquals(&d1))
-}
-
-func TestGreaterThanSamePrecScale(t *testing.T) {
-	d1 := createDecimal(123421, 10, 2)
-	d2 := createDecimal(123421, 10, 2)
-	d3 := createDecimal(456456, 10, 2)
-	require.True(t, d3.GreaterThan(&d1))
-	require.False(t, d1.GreaterThan(&d2))
-	require.False(t, d2.GreaterThan(&d1))
-}
-
-func TestGreaterThanDifferentPrecScale(t *testing.T) {
-	d1 := createDecimal(123421, 10, 2)
-	d2 := createDecimal(12342100, 13, 4)
-	d3 := createDecimal(22342100, 13, 4)
-	d4 := createDecimal(11342100, 13, 4)
-	require.False(t, d1.GreaterThan(&d2))
-	require.False(t, d2.GreaterThan(&d1))
-	require.False(t, d1.GreaterThan(&d3))
-	require.True(t, d1.GreaterThan(&d4))
-	require.False(t, d4.GreaterThan(&d1))
-}
-
-func TestGreaterOrEqualsSamePrecScale(t *testing.T) {
-	d1 := createDecimal(123421, 10, 2)
-	d2 := createDecimal(123421, 10, 2)
-	d3 := createDecimal(456456, 10, 2)
-	require.True(t, d3.GreaterOrEquals(&d1))
-	require.True(t, d1.GreaterOrEquals(&d2))
-	require.True(t, d2.GreaterOrEquals(&d1))
-	require.True(t, d3.GreaterOrEquals(&d1))
-}
-
-func TestGreaterOrEqualsDifferentPrecScale(t *testing.T) {
-	d1 := createDecimal(123421, 10, 2)
-	d2 := createDecimal(12342100, 13, 4)
-	d3 := createDecimal(22342100, 13, 4)
-	d4 := createDecimal(11342100, 13, 4)
-	require.True(t, d1.GreaterOrEquals(&d2))
-	require.True(t, d2.GreaterOrEquals(&d1))
-	require.False(t, d1.GreaterOrEquals(&d3))
-	require.True(t, d1.GreaterThan(&d4))
-	require.False(t, d4.GreaterOrEquals(&d1))
-}
-
-func TestAddSameScale(t *testing.T) {
-	d1 := createDecimal(123421, 10, 2)
-	d2 := createDecimal(211233, 10, 2)
-	dr, err := d1.Add(&d2)
-	require.NoError(t, err)
-	require.Equal(t, createDecimal(334654, 10, 2), dr)
-}
-
-func TestAddDifferentScale(t *testing.T) {
-	d1 := createDecimal(123421456, 10, 5)
-	d2 := createDecimal(211233, 10, 2)
-	dr, err := d1.Add(&d2)
-	require.NoError(t, err)
-	require.Equal(t, createDecimal(334654456, 10, 5), dr)
-}
-
-func TestSubtractSameScale(t *testing.T) {
-	d1 := createDecimal(123421, 10, 2)
-	d2 := createDecimal(211233, 10, 2)
-	dr, err := d2.Subtract(&d1)
-	require.NoError(t, err)
-	require.Equal(t, createDecimal(87812, 10, 2), dr)
-}
-
-func TestSubtractDifferentScale(t *testing.T) {
-	d1 := createDecimal(123421456, 10, 5)
-	d2 := createDecimal(211233, 10, 2)
-	dr, err := d2.Subtract(&d1)
-	require.NoError(t, err)
-	require.Equal(t, createDecimal(87811544, 10, 5), dr)
-}
-
-func TestMultiplySameScale(t *testing.T) {
-	d1 := createDecimal(123421, 38, 2)
-	d2 := createDecimal(211233, 38, 2)
-	dr, err := d1.Multiply(&d2)
-	require.NoError(t, err)
-	require.Equal(t, createDecimal(26070588093, 38, 4), dr)
-}
-
-func TestMultiplyDifferentScale(t *testing.T) {
-	d1 := createDecimal(123421765, 38, 5)
-	d2 := createDecimal(211233, 38, 2)
-	dr, err := d1.Multiply(&d2)
-	require.NoError(t, err)
-	require.Equal(t, createDecimal(26070749686245, 38, 7), dr)
-}
-
-func TestDivide(t *testing.T) {
-	d1 := createDecimal(333333, 38, 2)
-	d2 := createDecimal(300, 38, 2)
-	dr, err := d1.Divide(&d2)
-	require.NoError(t, err)
-	require.Equal(t, createDecimal(111111, 38, 2), dr)
-
-	d1 = createDecimal(444444, 38, 2)
-	d2 = createDecimal(200, 38, 2)
-	dr, err = d1.Divide(&d2)
-	require.NoError(t, err)
-	require.Equal(t, createDecimal(222222, 38, 2), dr)
-
-	d1 = createDecimal(1111, 38, 2)
-	d2 = createDecimal(25, 38, 2)
-	dr, err = d1.Divide(&d2)
-	require.NoError(t, err)
-	require.Equal(t, createDecimal(4444, 38, 2), dr)
-
-	d1 = createDecimal(333333, 38, 5)
-	d2 = createDecimal(300, 38, 2)
-	dr, err = d1.Divide(&d2)
-	require.NoError(t, err)
-	require.Equal(t, createDecimal(111111, 38, 5), dr)
-
-	d1 = createDecimal(1111, 38, 4)
-	d2 = createDecimal(25, 38, 2)
-	dr, err = d1.Divide(&d2)
-	require.NoError(t, err)
-	require.Equal(t, createDecimal(4444, 38, 4), dr)
-
-	d1 = createDecimal(1111, 38, 4)
-	d2 = createDecimal(25, 38, 2)
-	dr, err = d1.Divide(&d2)
-	require.NoError(t, err)
-	require.Equal(t, createDecimal(4444, 38, 4), dr)
+				dr, err = d1.Divide(&d2)
+				require.NoError(t, err, "%s / %s", d1.String(), d2.String())
+				require.Equal(t, tc.want.Divide, dr, "%s / %s == %s", d1.String(), d2.String(), dr.String())
+			},
+		)
+	}
 }
 
 func TestAddDoesNotFitInPrecision(t *testing.T) {
-	d1 := createDecimal(923421, 6, 2)
-	d2 := createDecimal(911233, 6, 2)
+	d1 := Dec(923421, 6, 2)
+	d2 := Dec(911233, 6, 2)
 	_, err := d1.Add(&d2)
 	require.Error(t, err)
 	require.Equal(t, "result of decimal arithmetic does not fit in precision 6", err.Error())
 }
 
 func TestSubtractDoesNotFitInPrecision(t *testing.T) {
-	d1 := createDecimal(923421, 6, 2)
-	d2 := createDecimal(911233, 6, 2)
+	d1 := Dec(923421, 6, 2)
+	d2 := Dec(911233, 6, 2)
 	d2.Num = d2.Num.Negate()
 	_, err := d1.Subtract(&d2)
 	require.Error(t, err)
@@ -278,46 +327,46 @@ func TestSubtractDoesNotFitInPrecision(t *testing.T) {
 }
 
 func TestMultiplyDoesNotFitInPrecision(t *testing.T) {
-	d1 := createDecimal(123421, 6, 2)
-	d2 := createDecimal(211233, 6, 2)
+	d1 := Dec(123421, 6, 2)
+	d2 := Dec(211233, 6, 2)
 	_, err := d1.Multiply(&d2)
 	require.Error(t, err)
 	require.Equal(t, "result of decimal arithmetic does not fit in precision 6", err.Error())
 }
 
 func TestShiftLeft(t *testing.T) {
-	d1 := createDecimal(123421, 38, 2)
+	d1 := Dec(123421, 38, 2)
 	dr := d1.Shift(4, false)
-	require.Equal(t, createDecimal(1234210000, 38, 2), dr)
+	require.Equal(t, Dec(1234210000, 38, 2), dr)
 }
 
 func TestShiftRight(t *testing.T) {
-	d1 := createDecimal(123457, 38, 2)
+	d1 := Dec(123457, 38, 2)
 	dr := d1.Shift(-2, false)
-	require.Equal(t, createDecimal(1234, 38, 2), dr)
+	require.Equal(t, Dec(1234, 38, 2), dr)
 	dr = d1.Shift(-2, true)
-	require.Equal(t, createDecimal(1235, 38, 2), dr)
+	require.Equal(t, Dec(1235, 38, 2), dr)
 }
 
 func TestToFloat64(t *testing.T) {
-	d := createDecimal(211233, 38, 2)
+	d := Dec(211233, 38, 2)
 	require.Equal(t, 2112.33, d.ToFloat64())
 
-	d = createDecimal(211233, 38, 0)
+	d = Dec(211233, 38, 0)
 	require.Equal(t, float64(211233), d.ToFloat64())
 
-	d = createDecimal(125, 38, 5)
+	d = Dec(125, 38, 5)
 	require.Equal(t, 0.00125, d.ToFloat64())
 }
 
 func TestToInt64(t *testing.T) {
-	d := createDecimal(211233, 38, 2)
+	d := Dec(211233, 38, 2)
 	require.Equal(t, int64(2112), d.ToInt64())
 
-	d = createDecimal(211233, 38, 0)
+	d = Dec(211233, 38, 0)
 	require.Equal(t, int64(211233), d.ToInt64())
 
-	d = createDecimal(1234, 38, -2)
+	d = Dec(1234, 38, -2)
 	require.Equal(t, int64(123400), d.ToInt64())
 }
 
@@ -412,7 +461,7 @@ func TestConvertPrecisionAndScale(t *testing.T) {
 	require.Equal(t, "123456.6543210000", d2.String())
 }
 
-func createDecimal(lo int64, prec int, scale int) Decimal {
+func Dec(lo int64, prec int, scale int) Decimal {
 	var num decimal128.Num
 	if lo >= 0 {
 		num = decimal128.New(0, uint64(lo))
